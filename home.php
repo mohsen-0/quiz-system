96ed96c25b8823a405bbaa7db9fa520d863edd2e
m0hsn <mohsen-0>
2024-12-11 21:46:14 +0200

    part 2

---
+++ home.php
@@ -0,0 +1,7 @@
+<?php
+require "auth.php";
+require "db.php";
+$uid=$_SESSION['SESS_MEMBER_ID'];
+echo $uid;
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+
+unset($_SESSION['SESS_MEMBER_ID']);
+
+require "connection.php";
+
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+
+    $str = "SELECT * FROM user WHERE email = ? AND password = ?";
+    $stmt = mysqli_prepare($conn, $str);
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($result) != 1) {
+        echo "<center><h3><script>alert('Invalid Username or Password')</script></h3></center>";
+        header("refresh:0;url=index.php");
+        exit();
+    } else {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['SESS_MEMBER_ID'] = $row['uid'];
+
+        header('Location: home.php');
+        exit();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +37,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href="styles.css" rel="stylesheet">
+    <link href="assets/styles.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </head>
@@ -21,7 +53,7 @@
                         <center>
                             <h4 style="font-family: 'Noto Sans';"><b>Login to Quiz</b></h4>
                         </center>
-                        <form method="post" action="login_process.php">
+                        <form method="post" action="">
                             <div class="form-group">
                                 <label for="email">Email</label>
                                 <input type="email" class="form-control text-lowercase" id="email" name="email" required>
@@ -30,7 +62,7 @@
                                 <label for="password">Password</label>
                                 <input type="password" class="form-control" id="password" name="password" required>
                             </div>
-                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                            <button type="submit" name="submit" class="btn btn-primary btn-block">Login</button>
                         </form>
                         <div class="text-center mt-3">
                             <a href="register.php">Don't have an account? Register here</a>
